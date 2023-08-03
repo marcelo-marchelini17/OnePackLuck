971fdc7f00a4c59327aa718d429227aef35cbe2e
Marcelo Andrade <136126835+marcelo-marchelini17@users.noreply.github.com>
2023-08-03 12:24:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='OnePackLuck',
+    version='0.1',
+    packages=['modulo'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'astropy'
+        # Agrega aquí las dependencias necesarias
+    ],
+)
