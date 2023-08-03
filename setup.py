9947926aca9ae9666aae3de320eeaa0486f55f0d
Marcelo Andrade <136126835+marcelo-marchelini17@users.noreply.github.com>
2023-08-03 12:39:15 -0400

    Update setup.py
    Se completa el setup.

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='procesamiento_imagenes_astro',
-    version='0.1.0',
-    packages=[''],
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='OnePackLuck',
+    version='0.1',
+    authors='Marcelo Andrade, Marc Valduz',
+    description='Paquete para el procesamiento de imágenes astronómicas',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/marcelo-marchelini17OnePackLuck',
+    packages=setuptools.find_packages(),
     install_requires=[
         'numpy',
         'matplotlib',
         'astropy',
-        'Pillow'
+        'Pillow',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
