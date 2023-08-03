6e0bab3234e268fdc77e3974c6e3c517a779871e
Marcelo Andrade <136126835+marcelo-marchelini17@users.noreply.github.com>
2023-08-03 15:57:25 -0400

    Update setup.py
    Se agrega los autores y url del repositorio

--- OnePackLuck/setup.py
+++ OnePackLuck/setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='imagen_processing',
     version='0.1',
+    description='Procesamiento de imágenes FITS',
+    author = 'MArcelo Andrade, Marc Valduz'
+    url = 'https://github.com/marcelo-marchelini17/OnePackLuck',
     packages=find_packages(),
     install_requires=[
         'numpy',
